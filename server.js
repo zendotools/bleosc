a71c63a77e39a5c29286424457031ae951666638
dpurdy <doug@purdy.im>
2019-11-27 11:58:03 -0800

    scan for movesense

--- server.js
+++ server.js
@@ -1,1 +1,30 @@
-console.log("bleosc")
+//https://github.com/noble/noble/wiki/Getting-started
+
+let noble = require("noble")
+
+noble.on('stateChange', function(state) {
+
+  print(state)
+  
+    if (state === 'poweredOn') 
+    {
+      print('bluetooth on')
+      let serviceUUIDs = ["0000fe06-0000-1000-8000-00805f9b34fb"]
+
+      noble.startScanning(serviceUUIDs);
+    } 
+    else 
+    {
+      print('bluetooth off')
+      noble.stopScanning();
+    }
+  });
+  
+  noble.on('discover', function(peripheral) {
+      console.log('Found device with local name: ' + peripheral.advertisement.localName);
+      //console.log('advertising the following service uuid\'s: ' + peripheral.advertisement.serviceUuids);
+      console.log();
+  });
+
+
+
